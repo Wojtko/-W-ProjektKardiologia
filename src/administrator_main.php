a4b19fe913f02d49a9cf6a969d0bfbff7e4fb539
Wojciech Adrych <wojtek.adrych@gmail.com>
2017-05-02 10:14:43 +0200

    Start

---
+++ src/administrator_main.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+
+	<meta charset="UTF-8">
+	<title>Protal kardiologiczny</title>
+
+	<meta name="descricption" content="Portal ułatwiający kontakt pomiędzy lekarzami, a chirurgami."/>
+	<meta name="keywords" content="lekarze, kariochirurg, kardiolog, pacjent, choroba, serce, medycyna">
+	<meta http-equiv = "X-UA-Compatible" content = "IE=edge,chrome=1" type = "text/css"/>
+
+	<link rel="stylesheet" href="style_admin.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="admin_patient_data.js"></script>
+
+</head> 
+<body>
+	
+	<div class="table">
+		<table>
+			<thead>
+				<tr>
+					<th><a href="#" style="color: #EF2D56;">Główna</a></th>
+					<th><a href="#">Kardiolodzy</a></th>
+					<th><a href="#">Pacjenci</a></th>
+				</tr>
+			</thead>
+		</table>
+	</div>
+
+	<div class="main">
+		<table>
+			<thead>
+				<tr>
+					<th>
+						<div class="btn" data-panelid="1">
+							<table>
+								<tr><th><b>Imię: </b>Jan</th> <th><b>Nazwisko:</b>Kowalski</th></tr>
+							</table>
+						</div>
+
+						<div class="data" id="data1">
+							<table>
+								<thead>
+									<tr>
+										<th><b>Data urodzenia:</b></th>
+										<th>12.06.1978</th>
+									</tr>
+									<tr>
+										<th><b>Adres:</b></th>
+										<th>ul.Wiejska 13 83-000 Gdańsk</th>
+									</tr>
+									<tr>
+										<th><b>Ropoznanie:</b></th>
+										<th>Chory ma niesprawne serce.</th>
+									</tr>
+									<tr>
+										<th colspan="2">TU BĘDZIE MOŻNA POBRAĆ PLIK</th>
+									</tr>
+								</thead>
+							</table>
+						</div>
+					</th>
+				</tr>
+			</thead>
+			<thead>
+				<tr>
+					<th>
+						<div class="btn" data-panelid="2">
+							<table>
+								<tr><th><b>Imię: </b>Jan</th> <th><b>Nazwisko:</b>Kowalski</th></tr>
+							</table>
+						</div>
+
+						<div class="data" id="data2">
+							<table>
+								<thead>
+									<tr>
+										<th><b>Data urodzenia:</b></th>
+										<th>12.06.1978</th>
+									</tr>
+									<tr>
+										<th><b>Adres:</b></th>
+										<th>ul.Wiejska 13 83-000 Gdańsk</th>
+									</tr>
+									<tr>
+										<th><b>Ropoznanie:</b></th>
+										<th>Chory ma niesprawne serce.</th>
+									</tr>
+									<tr>
+										<th colspan="2">TU BĘDZIE MOŻNA POBRAĆ PLIK</th>
+									</tr>
+								</thead>
+							</table>
+						</div>
+					</th>
+				</tr>
+			</thead>
+		</table>
+	</div>
+	
+</body>
+</html>
---
+++ src/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+
+	<meta charset="UTF-8">
+	<title>Protal kardiologiczny</title>
+
+	<meta name="descricption" content="Portal ułatwiający kontakt pomiędzy lekarzami, a chirurgami."/>
+	<meta name="keywords" content="lekarze, kariochirurg, kardiolog, pacjent, choroba, serce, medycyna">
+	<meta http-equiv = "X-UA-Compatible" content = "IE=edge,chrome=1" type = "text/css"/>
+
+	<link rel="stylesheet" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+</head> 
+<body>
+	
+	<h1>Portal Kardiologiczny SercMed</h1>
+
+	<div class="main">
+		<div class="login">
+			<form action="login.php" method="post">
+				Podaj login <br/>
+				<input type="text" name="login"> <br/> <br/>
+				Podaj hasło<br/>
+				<input type="password" name="passowrd"> <br/> <br/>
+				<input type="submit" value="Zaloguj się"> <br/>
+				<a href="registration.php">Nie masz jeszcze konta? Zarejestruj się już dziś!</a>
+			</form>
+		</div>
+	</div>
+
+</body>
+</html>
